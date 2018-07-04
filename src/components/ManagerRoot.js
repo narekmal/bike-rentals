dc6ddff5b6a24e5bad58a10c605440ca055adfe0
Narek Malkhasyan <narek.mal@gmail.com>
2018-07-05 00:18:34 +0400

    basic routing setup

--- src/components/ManagerRoot.js
+++ src/components/ManagerRoot.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import Selectable from '../lab/Selectable';
+import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
 import './ManagerRoot.css';
 
 class ManagerRoot extends Component {
   render() {
     return (
-      <div>
-        <div>this shows login info</div>
-        <div className="b-menu">
-          <a className="b-menu__item">Manage Bikes</a>
-          <a className="b-menu__item">Manage Users</a>
-          <a className="b-menu__item">Explore Reservations</a>
+      <Router>
+        <div>
+          <div>this shows login info</div>
+          <div className="b-menu">
+            <NavLink className="b-menu__item" to='/manage-bikes'>Manage Bikes</NavLink>
+            <NavLink className="b-menu__item" to='/manage-users'>Manage Users</NavLink>
+            <NavLink className="b-menu__item" to='/explore-reservations'>Explore Reservations</NavLink>
+          </div>
+          <div>
+            <Route path='/manage-bikes' render={()=>(<h1>manage bikes</h1>)}></Route>
+          </div>
         </div>
-        <Selectable data={[{first: "test1", second: "test3"}, {first: "test3"}]}></Selectable>
-      </div>
+      </Router>
+      
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Router, Route, hashHistory} from 'react-router';
 import ManagerRoot from './components/ManagerRoot';
 import registerServiceWorker from './registerServiceWorker';
 
