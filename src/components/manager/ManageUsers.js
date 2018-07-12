d851e4c830f33d77e02b2e0e507dce31a941c250
Narek Malkhasyan <narek.mal@gmail.com>
2018-07-12 15:16:53 +0400

    user reservations

--- src/components/manager/ManageUsers.js
+++ src/components/manager/ManageUsers.js
@@ -5,6 +5,11 @@ export default class ManageUsers extends Component {
   state = {
     editingItemId: -1, // -1 no editing, 0 adding
     tempItem: {...this.emptyItem},
+    reservations: {
+      itemId: -1,
+      loading: false,
+      data: []
+    },
     items: []
   }
 
@@ -14,11 +19,16 @@ export default class ManageUsers extends Component {
     let tableHeaderCells, tableRows = [];
     if(this.state.items.length !== 0){
       tableHeaderCells = Object.keys(this.state.items[0]).map((k,i)=><div key={i} className="b-table__header-cell">{k}</div>);
+      tableHeaderCells.push(<div key="-2" className="b-table__header-cell">Reservations</div>);
       tableHeaderCells.push(<div key="-1" className="b-table__header-cell">Actions</div>);
       tableRows = this.state.items.map(b=>{
         if(b.id === this.state.editingItemId)
           return this.getEditRow();
         let cells = Object.values(b).map((v,i)=><div key={i} className="b-table__cell">{v}</div>);
+        cells.push(
+          <div key="-2" className="b-table__cell">
+            <a className="u-link" onClick={()=>this.watchReservations(b.id)}>watch</a>
+          </div>);
         cells.push(
           <div key="-1" className="b-table__cell">
             <a className="u-link" onClick={()=>this.editItem(b.id)}>edit</a>
@@ -29,13 +39,42 @@ export default class ManageUsers extends Component {
       if(this.state.editingItemId === 0) // adding item
         tableRows.push(this.getEditRow());
     }
+
+    let resTableHeaderCells, resTableRowCells;
+    if(this.state.reservations.itemId > 0 && this.state.reservations.data.length > 0){
+      resTableHeaderCells = Object.keys(this.state.reservations.data[0]).map((k,i)=><div key={i} className="b-table__header-cell">{k}</div>);
+      resTableRowCells = this.state.reservations.data.map(r=>{
+        let cells = Object.values(r).map((v,i)=><div key={i} className="b-table__cell">{v}</div>);
+        return cells;
+      });
+    }
+
     return (
       <div className="l-pane">
-        <div className="b-table" style={{gridTemplateColumns: 'repeat(5, 1fr)'}}>
+        <div className="b-table" style={{gridTemplateColumns: 'repeat(6, 1fr)'}}>
           {tableHeaderCells}
           {tableRows}
         </div>
         <div style={{textAlign: "left", marginTop: 25}}><a className="u-link" onClick={()=>this.addItem()}>add user</a></div>
+
+        {this.state.reservations.itemId > 0 && 
+        <div className="b-reservations">
+          <div className="b-reservations__inner">
+            <div className="b-reservations__title">Reservations for user #{this.state.reservations.itemId}</div>
+            {this.state.reservations.loading && 
+            <div className="b-reservations__loading">loading...</div>}
+            {!this.state.reservations.loading && this.state.reservations.data.length === 0 &&
+            <div className="b-reservations__no-data">No reservations found</div>}
+            {!this.state.reservations.loading && this.state.reservations.data.length > 0 &&
+            <div className="b-reservations__table">
+              <div className="b-table" style={{gridTemplateColumns: 'repeat(3, 1fr)'}}>
+                {resTableHeaderCells}
+                {resTableRowCells}
+              </div>
+            </div>}
+          </div>
+        </div>}
+
       </div>
     )
   }
@@ -47,6 +86,7 @@ export default class ManageUsers extends Component {
         <div className="b-table__cell"><input type="text" value={this.state.tempItem.name} onChange={e=>{e.persist(); this.setState(s=>({tempItem:{...s.tempItem, name: e.target.value}}))}}/></div>
         <div className="b-table__cell"><input type="text" value={this.state.tempItem.password} onChange={e=>{e.persist(); this.setState(s=>({tempItem:{...s.tempItem, password: e.target.value}}))}}/></div>
         <div className="b-table__cell"><input type="text" value={this.state.tempItem.role} onChange={e=>{e.persist(); this.setState(s=>({tempItem:{...s.tempItem, role: e.target.value}}))}}/></div>
+        <div className="b-table__cell"></div>
         <div className="b-table__cell">
           <a className="u-link" onClick={()=>this.submitEditItem()}>submit</a>
           <a className="u-link" onClick={()=>this.setState({editingItemId: -1})}>cancel</a>
@@ -114,6 +154,21 @@ export default class ManageUsers extends Component {
             return s;
           });
         console.log(json)
+      });
+  }
+
+  watchReservations(id) {
+    this.setState(s=>({reservations:{...s.reservations, itemId: id, loading: true, data: []}}));
+
+    let formData = new FormData();
+    formData.append("userId", id);
+    fetch(`http://narek-dev.com/bike-rentals-api/api/getUserReservations`, {
+        method: 'POST',
+        body: formData
+      })
+      .then(res => res.json())
+      .then(json => {
+        this.setState(s=>({reservations: {...s.reservations, loading: false, data: json}}));
       });
   }
 
