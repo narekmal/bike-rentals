f95a6423b79ed88c98600024f886bb935accf152
Narek Malkhasyan <narek.mal@gmail.com>
2018-07-15 22:09:49 +0400

    filters initial setup

--- src/components/user/Filters.js
+++ src/components/user/Filters.js
@@ -11,11 +11,19 @@ const mapDispatchToProps = () => ({});
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)( class Filters extends Component {
 
   render() {
-
+    //color, model, weight, location or rate averages
     
 
     return (
-      <div className="b-filters">filters</div>
+      <div className="b-filters">
+        Filters
+        <form className="b-filters__form">
+          <input type="text" placeholder="Color"/>
+          <input type="text" placeholder="Model"/>
+        </form>
+        <button>Apply</button>
+        <button>Clear</button>
+      </div>
     )
   }
 
